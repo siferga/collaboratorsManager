47463c94dd789039f4604828d6d234ae0cfa9f7f
Siferga <sifergadeveloper@gmail.com>
2024-04-28 14:53:45 +0200

    Collaborator class

--- src/main/java/com/siferga/collaboratorsManager/model/Collaborator.java
+++ src/main/java/com/siferga/collaboratorsManager/model/Collaborator.java
@@ -19,4 +19,72 @@ public class Collaborator implements Serializable {
     private String colCode;
 
 
+    public Collaborator() {
+    }
+    public Collaborator(String name, String email, String jobTitle, String colPhone, String imageUrl, String colCode) {
+        this.name = name;
+        this.email = email;
+        this.jobTitle = jobTitle;
+        this.colPhone = colPhone;
+        this.imageUrl = imageUrl;
+        this.colCode = colCode;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public String getColPhone() {
+        return colPhone;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getColCode() {
+        return colCode;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public void setColPhone(String colPhone) {
+        this.colPhone = colPhone;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setColCode(String colCode) {
+        this.colCode = colCode;
+    }
+
+
 }
