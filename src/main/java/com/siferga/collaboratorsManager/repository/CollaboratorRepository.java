750f43a1dc9033fdc21ad8f0e4352904a4a9caac
Siferga <sifergadeveloper@gmail.com>
2024-04-28 21:33:42 +0200

    repository interface

--- src/main/java/com/siferga/collaboratorsManager/repository/CollaboratorRepository.java
+++ src/main/java/com/siferga/collaboratorsManager/repository/CollaboratorRepository.java
@@ -3,5 +3,10 @@ package com.siferga.collaboratorsManager.repository;
 import com.siferga.collaboratorsManager.model.Collaborator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.UUID;
+
 public interface CollaboratorRepository extends JpaRepository<Collaborator, Long> {
+    void deleteCollaboratorById(Long id);
+
+    Collaborator findCollaboratorById(Long id);
 }
